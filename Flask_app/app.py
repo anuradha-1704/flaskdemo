6e0b10c10074d1f124de56114611dc6ea6f471ee
Singh <695592@cognizant.com>
2021-09-01 22:02:52 +0530

    created templete

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -3,5 +3,17 @@ from flask import Flask
 # initilize the application
 app= Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "Hello World:"
+
+@app.route('/home')
+def home():
+    return "Home Page:"
+
+@app.route('/contact')
+def contact():
+    return "Contact Page:"
+
 if __name__=='__main__':
     app.run()
